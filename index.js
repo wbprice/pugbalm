f86f3abd2f35cdc5aa5b5f29ec9054fc43401158
Blaine Price <1wbprice@gmail.com>
2015-07-10 10:03:41 -0400

    Refactor using streams.

--- index.js
+++ index.js
@@ -10,23 +10,20 @@
  * node pugbalm 5 // download 5 pugs
  */
 
-var request = require('request'),
-    http = require('http'),
+var http = require('http'),
     _ = require('lodash'),
+    Stream = require('stream').Transform,
     fs = require('fs'),
 
     baseUrl = 'http://api.giphy.com/v1/gifs/search',
     apiKey = 'dc6zaTOxFJmzC',
-    params,
-    imageIds = [],
-    imageUrls = [];
 
-params = {
-    q: 'pugs',
-    limit: process.argv[2] || 5,
-    fmt: 'json',
-    api_key: apiKey,
-};
+    params = {
+        q: 'pugs',
+        limit: process.argv[2] || 5,
+        fmt: 'json',
+        api_key: apiKey,
+    };
 
 /*
  * @jsdoc function
@@ -54,67 +51,53 @@ function serializeParams(params) {
 
 }
 
-/*
-request(baseUrl + serializeParams(params), function(error, response, body) {
-
-    if (!error && response.statusCode === 200) {
-
-        body = JSON.parse(body).data;
-
-        _.forEach(body, function(element, index) {
-
-            imageIds.push(element.id);
-            imageUrls.push(element.images.original.url);
-
-        });
-
-        _.forEach(imageUrls, function(url, index) {
-
-            request({url: url, encoding: null}, function(error, response, body) {
-
-                var destination = process.cwd() + '/' + imageIds[index] + '.gif';
-
-                fs.writeFile(destination, body, 'binary', function(err) {
-
-                    if (err) throw err;
-
-                });
-
-            });
-
-        });
-
-        console.log(process.argv[2] + ' pugs were delivered to ' + __dirname + '.  \nPowered by GIPHY http://giphy.com/');
-                                                                       
-    }
-});
-*/
-
 function downloadImage(path, id) {
 
-    console.log('path: ' + path);
-    console.log('id: ' + id);
+    http.get(path, function(response) {
+
+      var output = new Stream(),
+          destination = process.cwd() + '/' + id + '.gif';
+
+      response.on('data', function(body) {
+        output.push(body);;
+
+      });
+
+      response.on('end', function() {
+        fs.writeFile(destination, output.read(), function(err) {
+          if (err) throw err;
+
+        });
+
+      });
+
+    });
 
 };
 
-http.get(baseUrl + serializeParams(params), function(response) {
+function getListOfImages(baseUrl, params) {
 
-    var output = '';
+  http.get(baseUrl + serializeParams(params), function(response) {
 
-    response.setEncoding('utf8');
-    response.on('data', function(body) {
+      var output = '';
 
-        output += body;
+      response.setEncoding('utf8');
+      response.on('data', function(body) {
+          output += body;
 
-    });
+      });
 
-    response.on('end', function() {
+      response.on('end', function() {
+      
+          output = JSON.parse(output).data;
 
-        output = output.data;
-        _.forEach(output, function(element) {
-            downloadImage(element.images.original.url, element.id);
-        });
+          _.forEach(output, function(element) {
+              downloadImage(element.images.original.url, element.id);
+          });
 
-    });
-});
+      });
+  });
 
+};
+
+getListOfImages(baseUrl, params);
